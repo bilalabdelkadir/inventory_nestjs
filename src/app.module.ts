a64decf136677bc167001907efb5683236ac4165
bilalabdelkadir <bilalbinabdelkadir>
2023-05-02 14:50:19 +0300

    changed email to unique

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [UserModule, ProductModule],
+  imports: [UserModule, ProductModule, PrismaModule, MailModule],
 })
 export class AppModule {}
